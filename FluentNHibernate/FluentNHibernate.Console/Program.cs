61d92ed5d9ec42c87289c992dc379ccdd3cb7d8e
YOUI\alex01w <alexw@youi.email>
2017-10-03 10:36:57 +1000

    starting the fluent nhibernate

---
+++ FluentNHibernate/FluentNHibernate.Console/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+
+namespace FluentNHibernate.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Configuration();
+            config.DataBaseIntegration(x =>
+            {
+                x.ConnectionString = "";
+                x.Driver<SqlClientDriver>();
+                x.Dialect<MsSql2012Dialect>();
+            });
+            config.AddAssembly(Assembly.GetExecutingAssembly());
+
+            var sessionaFactory = config.BuildSessionFactory();
+
+        }
+    }
+}
